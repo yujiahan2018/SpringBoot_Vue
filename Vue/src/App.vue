0ae0b112fac8c3a7863fd96d7a77308d4a1c9c6c
yujiahan <yujiahan_biancheng@163.com>
2022-08-06 22:22:39 +0800

    Vue

---
+++ Vue/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <inquire/>
+  </div>
+</template>
+
+<script>
+import inquire from './components/Inquire.vue'
+
+export default {
+  name: 'App',
+  components: {
+    inquire
+  },
+
+  
+}
+  
+</script>
+
---
+++ Vue/src/components/Inquire.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="sch">
+    <h1>查询数据库</h1>
+    <button @click="action">点击查询id为1的数据（结果控制台输出）</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'inquire',
+
+  methods:{
+    action(){
+      axios.get("http://localhost:8080/data?id=1").then(
+        response => {
+          console.log("@@@" , response.data)
+        },
+
+        err => {
+          console.log("%%%" , err)
+        }
+
+      )
+    }
+  }
+}
+</script>
+
+
+  
